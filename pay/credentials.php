5ad4ff031d98d36610bb6705657487d9d5afa53d
Brandon Tindle <bjtactor@gmail.com>
2018-10-20 16:59:13 -0400

    Added credentials and btn

---
+++ pay/credentials.php
@@ -0,0 +1,9 @@
+<?php 
+    define("PAYPAL_ACCOUNT_SAND", "andy_dold-facilitator@comcast.net");
+    define("PAYPAL_CLIENT_ID_SAND", "AU6LjAXAm60-wPuMDtjaqzaATFLG_pn2XTLSKHWtXLh0glv9Fjx26OfnVOqs-6MSqtF5wL_Xp_GYc2Y4");
+    define("PAYPAL_SECRET_SAND", "EHkgYNjVIjZspksMdZLuHGM2M-VZDSWAbbFeICWTVhSbJvFDwh5SPy4H7ZG-TviX-OTg2EoT0hUf4GfV");
+
+    define("PAYPAL_ACCOUNT_PROD", "admin@hoopcoach.org");
+    define("PAYPAL_CLIENT_ID_PROD", "AWkmSO1YdWEBL_2gCnO3WxGtYkOkVGzc4a7Z2utnLnNMaFijTcFE5E_woFrYDKrONT39sMmO8SJdIS35");
+    define("PAYPAL_SECRET_PROD", "EEu3e6FQiBxSMo9-d562pPD65JRPBAJl1JLzeS29G-pZTOnI4INCsWjO7f9GnTZ_EqFlemnJMRUTUxq2");
+?>
---
+++ pay/paypal_btn.php
@@ -0,0 +1,39 @@
+<?php include("./pay/credentials.php") ?>
+<div id="paypal-button"></div>
+<script src="https://www.paypalobjects.com/api/checkout.js"></script>
+<script>
+  paypal.Button.render({
+    // Configure environment
+    env: 'sandbox',
+    client: {
+      sandbox: 'PAYPAL_CLIENT_ID_SAND',
+      production: 'PAYPAL_CLIENT_ID_PROD'
+    },
+    // Customize button (optional)
+    locale: 'en_US',
+    style: {
+      size: 'small',
+      color: 'gold',
+      shape: 'pill',
+    },
+    // Set up a payment
+    payment: function(data, actions) {
+      return actions.payment.create({
+        transactions: [{
+          amount: {
+            total: '0.01',
+            currency: 'USD'
+          }
+        }]
+      });
+    },
+    // Execute the payment
+    onAuthorize: function(data, actions) {
+      return actions.payment.execute().then(function() {
+        // Show a confirmation message to the buyer
+        window.alert('Thank you for your purchase!');
+      });
+    }
+  }, '#paypal-button');
+
+</script>
